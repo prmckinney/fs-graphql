ee8a7062c8cb1f83a9cdea3d681a243221f82c11
Patrick McKinney <git@prmckinney.com>
2026-05-21 05:57:33 -0500

    Exercise 11

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import { gql } from "@apollo/client";
-import { useQuery } from "@apollo/client/react";
+import { useQuery, useMutation } from "@apollo/client/react";
 
 const GET_AUTHORS = gql`
   query AllAuthors {
@@ -12,8 +13,24 @@ const GET_AUTHORS = gql`
   }
 `;
 
+const EDIT_AUTHOR = gql`
+  mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+    }
+  }
+`;
+
 const Authors = (props) => {
+  const [name, setName] = useState("");
+  const [dateBorn, setDateBorn] = useState("");
+
   const result = useQuery(GET_AUTHORS);
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: GET_AUTHORS }],
+  });
+
   if (!props.show) {
     return null;
   }
@@ -24,25 +41,59 @@ const Authors = (props) => {
 
   const authors = result.data.allAuthors;
 
+  const submit = async (event) => {
+    event.preventDefault();
+
+    console.log("add book...");
+    editAuthor({
+      variables: { name, setBornTo: Number(dateBorn) },
+    });
+
+    setName("");
+    setDateBorn("");
+  };
+
   return (
     <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>born</th>
-            <th>books</th>
-          </tr>
-          {authors.map((a) => (
-            <tr key={a.id}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
+      <div>
+        <h2>authors</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>born</th>
+              <th>books</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            {authors.map((a) => (
+              <tr key={a.id}>
+                <td>{a.name}</td>
+                <td>{a.born}</td>
+                <td>{a.bookCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <h2>Set Birthyear</h2>
+        <form onSubmit={submit}>
+          <div>
+            name
+            <input
+              value={name}
+              onChange={({ target }) => setName(target.value)}
+            />
+          </div>
+          <div>
+            born
+            <input
+              value={dateBorn}
+              onChange={({ target }) => setDateBorn(target.value)}
+            />
+          </div>
+          <button type="submit">update author</button>
+        </form>
+      </div>
     </div>
   );
 };
