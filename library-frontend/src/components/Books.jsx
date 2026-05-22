a16cd699dcbbbd0c5abb5a705ff41207dcd68561
Patrick McKinney <git@prmckinney.com>
2026-05-22 13:07:05 -0500

    Exercise 18

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -5,7 +5,10 @@ const GET_BOOKS = gql`
   query AllBooks {
     allBooks {
       title
-      author
+      author {
+        name
+        born
+      }
       published
       id
     }
@@ -38,7 +41,7 @@ const Books = (props) => {
           {books.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
-              <td>{a.author}</td>
+              <td>{a.author.name}</td>
               <td>{a.published}</td>
             </tr>
           ))}
